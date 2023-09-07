11a74d3a491faec011d411f7c359b81d49c02590
runner <actions@noreply.luscii.com>
2023-09-07 15:05:42 +0000

    Updates Package.swift

--- Package.swift
+++ Package.swift
@@ -1,1 +1,25 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import PackageDescription
+
+let package = Package(
+    name: "Actions",
+    platforms: [
+        .iOS(.v14), 
+        .macOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "Actions",
+            targets: ["Actions"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Actions",
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/24/merge/Actions.xcframework.zip",
+            checksum: "0ad0fd9c892ab2171a461ef20015eac161b4e4b1a4c77003814a4cde74f2be1c"
+        )
+    ]
+)
