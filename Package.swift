1a6b0862f30a6753edc27ba961a67159396c5b8a
runner <actions@noreply.luscii.com>
2023-09-19 11:31:44 +0000

    Updates Package.swift and Actions.podspec

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Actions",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/27/merge/Actions.xcframework.zip",
-            checksum: "034d27ff97feaa8bce5638d751f526cf54acafe3e1caf2a37bc160a3e32752a7"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/0.2.0/Actions.xcframework.zip",
+            checksum: "61d7d9a8acf1abc9a8714545f87496b591f6fcce3d9f8b642bdf468c3a802369"
         )
     ]
 )
