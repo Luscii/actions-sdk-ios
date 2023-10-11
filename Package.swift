d3aa673bfcacb5379c3a88afaa749f80f9d3dd42
hungrxyz <marko@hungry.dev>
2023-10-11 09:35:37 +0200

    Adds 0.2.1 version info

--- Package.swift
+++ Package.swift
@@ -15,11 +15,20 @@ let package = Package(
             targets: ["Actions"]
         )
     ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.8.0"),
+        .package(url: "https://github.com/SVGKit/SVGKit.git", from: "3.0.0"),
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.4.0"),
+        .package(url: "https://github.com/Luscii/rxc-ios.git", from: "6.0.0"),
+        .package(url: "git@github.com:Luscii/httpii-ios.git", from: "0.1.0"),
+        .package(url: "git@github.com:Luscii/stiijl-ios.git", from: "0.2.0"),
+        .package(url: "git@github.com:Luscii/kraan-ios.git", from: "0.1.0")
+    ],
     targets: [
         .binaryTarget(
             name: "Actions",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/0.1.2/Actions.xcframework.zip",
-            checksum: "49bc0c406f818f0b4e1a9853bda61720cb762a8876f22c31900c0c58650acf5e"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/0.2.1/Actions.xcframework.zip",
+            checksum: "74e87cefd1bb1e12d24518c700217d0ce920cbdd35a869f9cac78f954d9abfac"
         )
     ]
 )
