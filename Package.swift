194d37d32c2a178720054d1fb996e6732e246b75
luscii-ios <actions@noreply.luscii.com>
2025-09-24 12:04:43 +0000

    Publish 2.0.0-beta.5

--- Package.swift
+++ Package.swift
@@ -16,23 +16,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Actions",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-beta.4/Actions.xcframework.zip",
-            checksum: "226714e90df02ea5ecfdf62e4b8da03d38e6c4e8f7239548771a73ed27287f0a"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-beta.5/Actions.xcframework.zip",
+            checksum: "4a95c545a504c22b9f0a1d505f73a9dc08934ca9408bfff22ca842db43c69630"
         ),
         .binaryTarget(
             name: "Centraal",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-beta.4/Centraal.xcframework.zip",
-            checksum: "92a8ced72c6e61747bc34ef8a48c53bd272355f838f785b9dc5c8b61e455c979"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-beta.5/Centraal.xcframework.zip",
+            checksum: "dd3e902bf294b142f2c6282e14a4b195b6789440cdf56271435dbce65e2f7919"
         ),
         .binaryTarget(
             name: "HTTPii",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-beta.4/HTTPii.xcframework.zip",
-            checksum: "ebf92e750fa95566a1bd7e3c7ec5b238c98aaf1e66546001d2331fa7cd4afef8"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-beta.5/HTTPii.xcframework.zip",
+            checksum: "889e4003d07b65e4c863795273056099a98c7d51b49f93ae98b3aeabb4b438f7"
         ),
         .binaryTarget(
             name: "Measurements",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-beta.4/Measurements.xcframework.zip",
-            checksum: "2b05202cccf38c1d851c65bdc372b907c750eb8be7f20ecdad559ae7bdcb3cdf"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-beta.5/Measurements.xcframework.zip",
+            checksum: "e5ded9c232f23a28f02ee0a9dc28e59f69ddb576f54d0f4da3e4ea68d9a8993f"
         ),
         .target(
             name: "ActionsSDK",
