f5a47a7f4ca2fba7df43fbb94c8dca30f9447464
luscii-ios <actions@noreply.luscii.com>
2025-09-30 13:31:31 +0000

    Publish 2.0.0-rc.4

--- Package.swift
+++ Package.swift
@@ -16,23 +16,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Actions",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-rc.3/Actions.xcframework.zip",
-            checksum: "1ed1205e0307eea94507a9931f873cf9ca39034641260520fb1b988fa7a4a28e"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-rc.4/Actions.xcframework.zip",
+            checksum: "e72b682f47c055940158968ceb241f673199114aabceba15fe7dd86db888bd26"
         ),
         .binaryTarget(
             name: "Centraal",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-rc.3/Centraal.xcframework.zip",
-            checksum: "66e436408d29822ed907a128d6f04de69e10620d32a91cb32b6202bdcf9899e3"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-rc.4/Centraal.xcframework.zip",
+            checksum: "384f168e0fa0249b7eb426397517f3da98f5555549c19e539abb931f1656c6df"
         ),
         .binaryTarget(
             name: "HTTPii",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-rc.3/HTTPii.xcframework.zip",
-            checksum: "52585ac7031de100316a2851e1ed6a65b7bd94abbf0de938a6a012a8def5dcab"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-rc.4/HTTPii.xcframework.zip",
+            checksum: "d1afc61ed094297baebd20adb4cde49dce5e72c471189766cb004e428f7fb018"
         ),
         .binaryTarget(
             name: "Measurements",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-rc.3/Measurements.xcframework.zip",
-            checksum: "76b59b13aee5863cdf54e9064b9ee67ca776d96b76de3877ee431a7780c70383"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.0.0-rc.4/Measurements.xcframework.zip",
+            checksum: "e1e5b9148c4f3e875c5aa522a802661e38e07825fe1b3a063ecdea29527c18db"
         ),
         .target(
             name: "ActionsSDK",
