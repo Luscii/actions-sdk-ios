b295b2c5b374c65af87a9c569adcbd5baa5a82da
luscii-ios <actions@noreply.luscii.com>
2026-07-01 14:47:34 +0000

    Publish 2.3.0-rc.1

--- Package.swift
+++ Package.swift
@@ -16,23 +16,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Actions",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.0-beta.2/Actions.xcframework.zip",
-            checksum: "b7c92c3797a82373fa9a7e58025102fa124142c100f3a806bd7d58a5710f8db2"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.0-rc.1/Actions.xcframework.zip",
+            checksum: "00d4d0a5280c97595dd8380b78819413bbc05cceb28978c276ce80973fc11a15"
         ),
         .binaryTarget(
             name: "Centraal",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.0-beta.2/Centraal.xcframework.zip",
-            checksum: "0937ec15befee8cfc6504fa8eea4a95d40f55587387dbf42bab805092a6b42af"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.0-rc.1/Centraal.xcframework.zip",
+            checksum: "99d6bd2d5c1f74244c2bb59d09e86123837b38718f188dbfd97f4c2e774eaf28"
         ),
         .binaryTarget(
             name: "HTTPii",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.0-beta.2/HTTPii.xcframework.zip",
-            checksum: "78a10f1490cfd1e1e1559e6f831af37c950c2bffead8be04d0b378a1155d605d"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.0-rc.1/HTTPii.xcframework.zip",
+            checksum: "6d57314dd98ea55042af0b69ec0b589eda0ca3302cb96f6a3e309f16586e407b"
         ),
         .binaryTarget(
             name: "Measurements",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.0-beta.2/Measurements.xcframework.zip",
-            checksum: "65160b21bae0f2e10c8b9da3645664bcf699f433e9aa13811d20e8983d6119e3"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.0-rc.1/Measurements.xcframework.zip",
+            checksum: "f75c7920444634dd732ea3a922290df6b3901f20b2f9dfc256b3d7363240cd07"
         ),
         .target(
             name: "ActionsKit",
