52111be04e899dcb66cf8392dbfcec1bad3431b2
luscii-ios <actions@noreply.luscii.com>
2026-08-10 16:27:27 +0000

    Publish 2.3.1

--- Package.swift
+++ Package.swift
@@ -16,23 +16,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Actions",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.1-rc.2/Actions.xcframework.zip",
-            checksum: "a8155d591c6782c845bae5ad3a256930e4cb68afba2f6f3e339e9c699590de31"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.1/Actions.xcframework.zip",
+            checksum: "ea14a6d4cc742d56b6842afe1ef79f13c91a71b15a8b41c52e4581fccb5bee81"
         ),
         .binaryTarget(
             name: "Centraal",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.1-rc.2/Centraal.xcframework.zip",
-            checksum: "87a419c9fef253190ad56d448d2d5acb915c180389f074e2cd64141b7bcf84ab"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.1/Centraal.xcframework.zip",
+            checksum: "5ce9d141dcc0a6199ff11bd1aa2690fa6faf7182bbaf73a0776e1e4db5a3c034"
         ),
         .binaryTarget(
             name: "HTTPii",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.1-rc.2/HTTPii.xcframework.zip",
-            checksum: "a0f75a48af9312117f799c5db88889a67c34a57a8903c5d51cc85ab22cd05564"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.1/HTTPii.xcframework.zip",
+            checksum: "8766072c8afed451158a0313cd74c83911bbbc4e984a83090fe55f74651c611a"
         ),
         .binaryTarget(
             name: "Measurements",
-            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.1-rc.2/Measurements.xcframework.zip",
-            checksum: "c03c6b69087679ea0c39edb98088fb0b4f6b6129734d4a55e937753c457b88c4"
+            url: "https://github.com/Luscii/actions-sdk-ios/releases/download/2.3.1/Measurements.xcframework.zip",
+            checksum: "f67854d50c6f8b50a6652edf92b0793e6696189452fa326a2f2ef6d70af7eedd"
         ),
         .target(
             name: "ActionsKit",
